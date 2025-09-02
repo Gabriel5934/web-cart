335f998cfe2bdaca3afa28c2c44113aeb70f946f
Gabriel5934 <gabriel.andrade@outlook.com.br>
2025-09-02 17:42:06 -0300

    add users

---
+++ app/users.ts
@@ -0,0 +1,107 @@
+export const users = [
+  {
+    username: "AnaEstela",
+    code: 2449,
+    displayName: "Ana Estela",
+  },
+  {
+    username: "AndreLeinio",
+    code: 9226,
+    displayName: "Andre Leinio",
+  },
+  {
+    username: "AngelaDavid",
+    code: 6954,
+    displayName: "Angela David",
+  },
+  {
+    username: "ArihaneElise",
+    code: 7533,
+    displayName: "Arihane Elise",
+  },
+  {
+    username: "CarolyneMateos",
+    code: 6829,
+    displayName: "Carolyne Mateos",
+  },
+  {
+    username: "ClaudiaRegina",
+    code: 8541,
+    displayName: "Claudia Regina",
+  },
+  {
+    username: "DanielaP",
+    code: 4168,
+    displayName: "Daniela P",
+  },
+  {
+    username: "FernandoAlberto",
+    code: 9201,
+    displayName: "Fernando Alberto",
+  },
+  {
+    username: "GabrielNunez",
+    code: 4281,
+    displayName: "Gabriel Nunez",
+  },
+  {
+    username: "GustavoHenrique",
+    code: 5015,
+    displayName: "Gustavo Henrique",
+  },
+  {
+    username: "HugoDario",
+    code: 5649,
+    displayName: "Hugo Dario",
+  },
+  {
+    username: "JulianaNakatsubo",
+    code: 5245,
+    displayName: "Juliana Nakatsubo",
+  },
+  {
+    username: "LuanaRoque",
+    code: 4050,
+    displayName: "Luana Roque",
+  },
+  {
+    username: "LuigiMoraes",
+    code: 3709,
+    displayName: "Luigi Moraes",
+  },
+  {
+    username: "MariaGracas",
+    code: 6118,
+    displayName: "Maria das Graças",
+  },
+  {
+    username: "MarisaSantos",
+    code: 3064,
+    displayName: "Marisa Santos",
+  },
+  {
+    username: "RobérioSena",
+    code: 3208,
+    displayName: "Robério Sena",
+  },
+  {
+    username: "SeigiYamauchi",
+    code: 4468,
+    displayName: "Seigi Yamauchi",
+  },
+  {
+    username: "ShirleyAnn",
+    code: 3298,
+    displayName: "Shirley Ann",
+  },
+  {
+    username: "VanessaHeloa",
+    code: 9064,
+    displayName: "Vanessa Heloa",
+  },
+  {
+    username: "WalterDias",
+    code: 1964,
+    displayName: "Walter Dias",
+  },
+];
